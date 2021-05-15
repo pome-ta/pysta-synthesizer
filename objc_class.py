54ac20070d02a349d13e854ab55bdc49c262ba61
pome-ta <pome-ta@users.noreply.github.com>
2021-05-15 22:05:28 +0900

    update

--- objc_class.py
+++
@@ -1,7 +0,0 @@
-from objc_util import ObjCClass
-
-AVAudioEngine = ObjCClass('AVAudioEngine')
-AVAudioFormat = ObjCClass('AVAudioFormat')
-AVAudioSourceNode = ObjCClass('AVAudioSourceNode')
-AVAudioUnitDelay = ObjCClass('AVAudioUnitDelay')
-
--- oscillator.py
+++
@@ -1,5 +0,0 @@
-class Oscillator:
-  def __init__(self, frequency: float=440.0, amplitude: float=1.0):
-    self.amplitude = amplitude
-    self.frequency = frequency
-
